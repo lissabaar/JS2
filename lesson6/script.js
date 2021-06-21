9da004becc1c635003b321437c6d48d5b113a744
Vasilisa <84107678+lissabaar@users.noreply.github.com>
2021-06-21 13:25:26 +0300

    Create script.js

---
+++ lesson6/script.js
@@ -0,0 +1,172 @@
+'use strict';
+
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+// ПОИСК
+
+Vue.component('search', {
+    data() {
+        return {
+            searchLine: '',
+        }
+    },
+    template: `<input type="text" class="goods-search" v-model="searchLine" @input="filterItems(searchLine)" placeholder="Поиск">`,
+    methods: {
+        filterItems(searchLine) {
+            const regexp = new RegExp(searchLine, 'i');
+            app.filteredItems = app.items.filter(item => regexp.test(item.product_name));
+            if (app.items.length == 0 || app.filteredItems.length == 0) {
+                app.isItemsListEmpty = true;
+            } else {
+                app.isItemsListEmpty = false;
+            }
+        },
+    }
+});
+
+// КОРЗИНА
+
+Vue.component('cart', {
+    props: ['items'],
+
+    template: `<div><button id="cart-btn" @click="cartClickHandler"></button>
+    <div id="user-cart" v-show="isVisibleCart">
+    <h2 class="cart-title">Корзина</h2>
+        <button id="cart-close" @click="closeCart">[x]</button>
+        <p v-if="!items.length">Корзина пуста.</p>
+        <div id="cart-items">
+        <cart-item v-for="item in items" :item="item"></cart-item>
+        </div>
+        <button @click="clear">Очистить корзину</button>
+        </div></div>`,
+
+    data() {
+        return {
+            isVisibleCart: false,
+        }
+    },
+
+    methods: {
+        cartClickHandler() {
+            if (this.isVisibleCart === true) {
+                this.closeCart();
+            }
+            else {
+                this.isVisibleCart = true;
+            }
+        },
+
+        closeCart() {
+            this.isVisibleCart = false;
+        },
+
+        clear() {
+            localStorage.removeItem('cartItems');
+            app.cartItems = [];
+        },
+    }
+})
+
+// ТОВАР В КОРЗИНЕ
+
+Vue.component('cart-item', {
+    props: ['item'],
+
+    template: `<div class="cart-item" v-if="item.count > 0">
+    <h3>{{ item.product_name }}</h3>
+    <p>Количество: <button class="count-btn" @click="countDown(item)">[-]</button> {{ item.count }} <button class="count-btn" @click="countUp(item)">[+]</button></p>
+    <p>Цена: {{ item.price * item.count }} руб.</p>
+  </div>`,
+
+    methods: {
+        countDown(item) {
+            item.count--;
+    
+            if (item.count <= 0) {
+                let itemToDelete = app.cartItems.indexOf(item);
+                app.cartItems.splice(itemToDelete, 1);
+                console.log(app.cartItems)
+            }
+
+            localStorage.setItem('cartItems', JSON.stringify(app.cartItems));
+
+        },
+
+        countUp(item) {
+            item.count++
+               localStorage.setItem('cartItems', JSON.stringify(app.cartItems));
+        },
+
+    }
+});
+
+// КАТАЛОГ
+
+Vue.component('catalog-list', {
+    props: ['items'],
+
+    template: `<div class="catalog-list">
+                <catalog-item v-for="item in items" :item="item"></catalog-item>
+                </div>`,
+});
+
+// ТОВАР
+
+Vue.component('catalog-item', {
+    props: ['item'],
+
+    template: `<div class="catalog-item">
+    <h3>{{ item.product_name }}</h3>
+    <p>{{ item.type }}</p>
+    <p>Цена: {{ item.price }} руб.</p>
+    <button class="catalog-item-button" @click="addToCart(item)">Добавить в корзину</button>
+  </div>`,
+
+    methods: {
+        addToCart(item) {
+            let isNew = true;
+            if (app.cartItems.length > 0) {
+                for (let i = 0; i < app.cartItems.length; i++) {
+                    if (item.id_product == app.cartItems[i].id_product) {
+                        app.cartItems[i].count++;
+                        isNew = false;
+                    }
+                }
+            }
+            if (isNew) {
+                app.cartItems.push({ ...item, 'count': 1 });
+            }
+            localStorage.setItem('cartItems', JSON.stringify(app.cartItems));
+        },
+    }
+});
+
+// ОШИБКА 
+
+Vue.component('catalog-error', {
+    template: `<p>Ошибка соединения. Повторите позднее</p>`,
+})
+
+// ВЬЮ
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        items: [],
+        filteredItems: [],
+        isError: false,
+        isItemsListEmpty: false,
+        cartItems: JSON.parse(localStorage.getItem('cartItems')) || [],
+    },
+    mounted() {
+        fetch(`${API_URL}/catalogData.json`)
+            .then((response) => response.json())
+            .then((data) => {
+                this.items = data;
+                this.filteredItems = data;
+            })
+            .catch(() => {
+                this.isError = true;
+            });
+    },
+});
